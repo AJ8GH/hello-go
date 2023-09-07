a10cfd2da724dab8e692555c5441b06e9ce590e3
Adam Jonas <adamjonas25@gmail.com>
2023-09-07 21:02:37 +0100

    Fix db access

--- data-access/main.go
+++ data-access/main.go
@@ -11,6 +11,13 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 func main() {
 	// Capture connection properties.
 	cfg := mysql.Config{
@@ -19,10 +26,10 @@ func main() {
 		Net:    "tcp",
 		Addr:   "127.0.0.1:3306",
 		DBName: "recordings",
-	}
-
-	// Get a database handle.
-	db, err := sql.Open("mysql", cfg.FormatDSN())
+		}
+		// Get a database handle.
+		var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,6 +38,60 @@ func main() {
 	if pingErr != nil {
 		log.Fatal(pingErr)
 	}
-
 	fmt.Println("Connected!")
+
+	insertData()
+
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
+
+func insertData() {
+	exec("DROP TABLE IF EXISTS album")
+	exec("CREATE TABLE album ( " +
+		"id     INT AUTO_INCREMENT NOT NULL, " +
+		"title  VARCHAR(128)  NOT NULL, " +
+		"artist VARCHAR(255)  NOT NULL, " +
+		"price  DECIMAL(5, 2) NOT NULL, " +
+		"PRIMARY KEY (`id`));")
+	exec(`INSERT INTO album
+	(title, artist, price)
+	VALUES ('Blue Train', 'John Coltrane', 56.99),
+	('Giant Steps', 'John Coltrane', 63.99),
+	('Jeru', 'Gerry Mulligan', 17.99),
+	('Sarah Vaughan', 'Sarah Vaughan', 34.98);`)
+}
+
+// albumsByArtist queries for albums that have the specified artist name.
+func albumsByArtist(name string) ([]Album, error) {
+	// An albums slice to hold data from returned rows.
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
+}
+
+func exec(query string) {
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
